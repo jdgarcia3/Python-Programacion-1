3b39a9718f249759d89319e9d2cc09ae1bf52fb5
Jesús García <jgarcia3@unisangil.edu.co>
2025-02-26 14:59:27 -0500

    transformaciones de variables y solicitud de informacion por terminal

---
+++ transformaciones.py
@@ -0,0 +1,16 @@
+#Nombre
+nombre = input("Ingrese su nombre: ")
+print(nombre)
+print(type(nombre))
+#Edad
+edad = int(input("Ingrese su edad: "))
+print(edad)
+print(type(edad))
+#Estado de matricula
+estado = bool(input("Estado de matricula: True/False"))
+print(estado)
+print(type(estado))
+#Peso
+peso = float(input("Ingrese su peso: "))
+print(peso)
+print(type(peso))
