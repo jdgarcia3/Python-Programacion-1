ef783058683f594645c0e894e00743d274e1b50d
Jesús García <jgarcia3@unisangil.edu.co>
2025-02-26 14:41:52 -0500

    tipos de variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+entero = 100
+print(type(entero))
+decimal = 10.3
+print(type(decimal))
+letra = 'a'
+print(type(letra))
+mensaje = "hola"
+print(type(mensaje))
+estado = False
+print(type(estado))
+parrafo = """
+Docente: Jesús David García Caro
+Correo: jgarcia3@unisangil.edu.co
+"""
+print(type(parrafo))
