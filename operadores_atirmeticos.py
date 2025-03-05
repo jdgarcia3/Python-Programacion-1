d31295e2b47694951408928de01db6deef08bbe7
Jesús García <jgarcia3@unisangil.edu.co>
2025-03-05 14:39:32 -0500

    ejemplos de operadores aritmeticos

--- operadores_atirmeticos.py
+++ operadores_atirmeticos.py
@@ -3,3 +3,12 @@ import math
 
 ejemplo1 = 10 + (2*(-3*((7-2)+(7*(3-2)))))
 print(ejemplo1)
+
+ejemplo2 = 2*(4*(7+4*(5*3-9))-3*(40-8))
+print(ejemplo2)
+
+ejemplo3 = 3*(4+(2*(3+5)))+2
+print(ejemplo3)
+
+ejemplo4 = 2*(4*(7+4*6)-3*32)
+print(f"ejemplo4 = {ejemplo4}")
