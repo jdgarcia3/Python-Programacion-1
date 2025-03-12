cb3aa9502f46393722dd59b37696f7747b6d20e3
Jesús García <jgarcia3@unisangil.edu.co>
2025-03-12 14:52:51 -0500

    Estructura de repetición While

---
+++ while.py
@@ -0,0 +1,12 @@
+#Variable op
+opcion = input("Ingrese una opción: ")
+#While
+while opcion != "salir":
+    print("dentro del bucle de repeticion")
+print("Fuera del bucle de repeticion")
+#While True
+while True:
+    opcion = input("Ingrese una opción: ")
+    if opcion == "salir":
+        break
+    print("dentro del bucle de repeticion")
