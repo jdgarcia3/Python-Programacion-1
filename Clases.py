277d3516267e2dd5fa1df1cb53b6a119f747ae7f
Jesús García <jgarcia3@unisangil.edu.co>
2025-05-14 15:52:04 -0500

    Se agrego clase estuduante

--- Clases.py
+++ Clases.py
@@ -39,3 +39,24 @@ BMW = Conche("BMW", "X5", "XYZ789", "Blanco")
 #Llamar a los metodos de la clase coche
 BMW.mostrar_informacion()
 Toyota.encender()
+
+#Definir clase estudiante
+class Estudiante:
+    def __init__(self, nombre, edad, carrera):
+        self.nombre = nombre
+        self.edad = edad
+        self.carrera = carrera
+
+    def mostrar_informacion(self):
+        print(f"Nombre: {self.nombre}, Edad: {self.edad}, Carrera: {self.carrera}")
+    
+    #metodo para estudiar
+    def estudiar(self):
+        print(f"{self.nombre} está estudiando {self.carrera}.")
+
+#Llamar a la clase estudiante
+Juan = Estudiante("Juan", 20, "Ingeniería")
+Pedro = Estudiante("Pedro", 22, "Medicina")
+#Llamar a los metodos de la clase estudiante
+Juan.mostrar_informacion()
+Pedro.estudiar()
