1cd3238648b61416f80ecd5bb39adf2f57e1d602
Jesús García <jgarcia3@unisangil.edu.co>
2025-05-14 15:36:25 -0500

    Definicion de estructura de clases

---
+++ Clases.py
@@ -0,0 +1,41 @@
+#Definir clase persona
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def mostrar_informacion(self):
+        print(f"Nombre: {self.nombre}, Edad: {self.edad}")
+    
+    #metodo para saludar
+    def saludar(self):
+        print(f"Hola, mi nombre es {self.nombre} y tengo {self.edad} años.")
+
+#Llamar a la clase persona
+Juan = Persona("Juan", 30)
+Juan.mostrar_informacion()
+Pedro = Persona("Pedro", 25)
+Pedro.mostrar_informacion()
+Juan.saludar()
+
+#Definir clase conche
+class Conche:
+    def __init__(self, marca, modelo, placa, color):
+        self.marca = marca
+        self.modelo = modelo
+        self.placa = placa
+        self.color = color
+
+    def mostrar_informacion(self):
+        print(f"Marca: {self.marca}, Modelo: {self.modelo}, Placa: {self.placa}, Color: {self.color}")
+    
+    #metodo para encender el coche
+    def encender(self):
+        print(f"El coche {self.marca} {self.modelo} está encendido.")
+
+#Llamar a la clase coche
+Toyota = Conche("Toyota", "Corolla", "ABC123", "Rojo")
+BMW = Conche("BMW", "X5", "XYZ789", "Blanco")
+#Llamar a los metodos de la clase coche
+BMW.mostrar_informacion()
+Toyota.encender()
